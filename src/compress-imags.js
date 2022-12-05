e8c6a271ba4cdf3797957a4648a54855b84b0d3c
komarov-sergey <komarovsmail33@gmail.com>
2022-12-05 10:59:57 +0300

    fet: remove some code

--- src/compress-imags.js
+++ src/compress-imags.js
@@ -8,14 +8,6 @@ const {
 } = require("./lib/virtualFS");
 const _ = require("lodash");
 
-const tree = mkdir("my documents", [
-  mkfile("avatar.jpg", { size: 100 }),
-  mkfile("passport.jpg", { size: 200 }),
-  mkfile("family.jpg", { size: 150 }),
-  mkfile("addresses", { size: 125 }),
-  mkdir("presentations"),
-]);
-
 function compressImages(tree) {
   const newTree = _.cloneDeep(tree);
   const isImg = ({ name }) => name.includes(".jpg");
@@ -27,8 +19,6 @@ function compressImages(tree) {
 
   return newTree;
 }
-
-compressImages(tree);
 
 module.exports = {
   compressImages,
